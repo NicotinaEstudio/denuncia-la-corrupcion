200e99beda916230650a48c02e21a9ef82cd61e5
Izakc <chernandez@nicotinaestudio.mx>
2014-09-29 08:03:50 -0500

    Alta de proyecto IOS

---
+++ DLC_app/IOS/DenunciaLaCorrupcion/DenunciaLaCorrupcion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DenunciaLaCorrupcion
+//
+//  Created by Isaac Hernández Morfín on 29/09/14.
+//  Copyright (c) 2014 Nicotina Estudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DLC_app/IOS/DenunciaLaCorrupcion/DenunciaLaCorrupcion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DenunciaLaCorrupcion
+//
+//  Created by Isaac Hernández Morfín on 29/09/14.
+//  Copyright (c) 2014 Nicotina Estudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
