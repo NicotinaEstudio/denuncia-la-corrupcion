92b9f17058602b89106a4e01eb86047ca1906f13
Izakc <chernandez@nicotinaestudio.mx>
2014-10-31 16:17:30 -0600

    inicial

--- DLC_app/IOS/DenunciaLaCorrupcion/Pods/Pods-DenunciaLaCorrupcion-BlurryModalSegue-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DenunciaLaCorrupcion_BlurryModalSegue : NSObject
-@end
-@implementation PodsDummy_Pods_DenunciaLaCorrupcion_BlurryModalSegue
-@end
--- DLC_app/IOS/DenunciaLaCorrupcion/Pods/Pods-DenunciaLaCorrupcion-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DenunciaLaCorrupcion : NSObject
-@end
-@implementation PodsDummy_Pods_DenunciaLaCorrupcion
-@end
